91985eab43da637a3126dfb5257df15b5dec0682
sky42 <sky2t3t7@gmail.com>
2019-06-15 06:20:25 +0100

    stop_flash.sh add fallback for iptables flush
    iptables needs to be flushed before start, so that https detections
    works as desired.

--- stop_flash.sh
+++ stop_flash.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 echo "Stopping AP in a screen"
+
+# let the setup_ap.sh script continue
+sudo pkill hostapd 2>/dev/null
+
+# just to be sure
+sudo iptables -F
+sudo iptables -F -t nat
+
+sleep 1
+
 sudo screen -S smarthack-wifi         -X stuff '^C'
 sudo screen -S smarthack-web          -X stuff '^C'
 sudo screen -S smarthack-smartconfig  -X stuff '^C'
